8153a59c9a6fe1db83482c109f9b32ab7d2c9b68
GaboSO21 <gso2090@gmail.com>
2024-11-13 19:32:22 -0600

    Started dasboard integration

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -3,11 +3,14 @@ from airflow.decorators import dag, task
 from airflow.hooks.base import BaseHook
 from airflow.sensors.base import PokeReturnValue
 from airflow.providers.docker.operators.docker import DockerOperator
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
 # from airflow.providers.pagerduty.notifications.pagerduty import send_pagerduty_notification
 from datetime import datetime
 import requests
 
-from include.stock_market.tasks import _get_stock_prices, _stock_prices, _get_formatted_csv
+from include.stock_market.tasks import _get_stock_prices, _stock_prices, _get_formatted_csv, BUCKET_NAME
 
 SYMBOL = "AAPL"
 
@@ -80,7 +83,20 @@ def stock_market():
         }
     )
 
-    is_api_available() >> get_stock_prices >> stock_prices >> format_prices >> get_formatted_csv
+    load_to_dw = aql.load_file(
+        task_id='load_to_dw',
+        input_file=File(
+            path=f"s3://{BUCKET_NAME}/{{task_instance.xcom_pull(task_ids='get_formatted_csv')}}", conn_id='minio'),
+        output_table=Table(
+            name='stock_market',
+            conn_id='postgres',
+            metadata=Metadata(
+                schema='public'
+            )
+        )
+    )
+
+    is_api_available() >> get_stock_prices >> stock_prices >> format_prices >> get_formatted_csv >> load_to_dw
 
 
 stock_market()
