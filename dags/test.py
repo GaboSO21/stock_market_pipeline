eb6af584ef023e362c56659a4f690b9a560a7842
GaboSO21 <gso2090@gmail.com>
2024-11-17 19:52:08 -0600

    DAG Backfilling

---
+++ dags/test.py
@@ -0,0 +1,21 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+
+@dag(
+    start_date=datetime(2024, 1, 1),
+    schedule="@daily",
+    catchup=True,
+    tags=["test"],
+)
+def retail():
+
+    @task
+    def start():
+        print("Hi")
+        raise ValueError("Error")
+
+    start()
+
+
+retail()
